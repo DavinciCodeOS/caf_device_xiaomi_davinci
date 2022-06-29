49bec89b581e5e83a9845faf1c95c02c063c0a8e
Diniboy1123 <Diniboy1123@users.noreply.github.com>
2022-06-29 11:31:03 +0300

    Revert "davinci: Rename camera faceunlock libmpbase"
    This reverts commit f4eada8322a14a8c4ff7aacc951ed62ddb6c8f35.

--- extract-files.sh
+++ extract-files.sh
@@ -13,18 +13,6 @@ function blob_fixup() {
             sed -i "s|libMegviiFacepp-0.5.2.so|libFaceDetectpp-0.5.2.so|g" "${2}"
             sed -i "s|megviifacepp_0_5_2_model|facedetectpp_0_5_2_model|g" "${2}"
             ;;
-        vendor/lib64/camera/components/com.arcsoft.node.capturebokeh.so | vendor/lib64/camera/components/com.arcsoft.node.capturefusion.so | \
-        vendor/lib64/camera/components/com.arcsoft.node.deflicker.so | vendor/lib64/camera/components/com.arcsoft.node.distortioncorrection.so | \
-        vendor/lib64/camera/components/com.arcsoft.node.hdr.so | vendor/lib64/camera/components/com.arcsoft.node.hdrchecker.so | \
-        vendor/lib64/camera/components/com.arcsoft.node.realtimebokeh.so | vendor/lib64/camera/components/com.arcsoft.node.smooth_transition.so | \
-        vendor/lib64/camera/components/com.arcsoft.node.superlowlight.so | vendor/lib64/libarcsat.so | vendor/lib64/libarcsoft_beautyshot.so | \
-        vendor/lib64/libarcsoft_bodyslim.so | vendor/lib64/libarcsoft_distortion_correction.so | vendor/lib64/libarcsoft_dualcam_image_optical_zoom.so | \
-        vendor/lib64/libarcsoft_dualcam_refocus.so | vendor/lib64/libarcsoft_dualcam_refocus_front.so | vendor/lib64/libarcsoft_dualcam_refocus_rear_t.so | \
-        vendor/lib64/libarcsoft_dualcam_refocus_rear_w.so | vendor/lib64/libarcsoft_high_dynamic_range.so | vendor/lib64/libarcsoft_low_light_hdr.so | \
-        vendor/lib64/libarcsoft_portrait_lighting.so | vendor/lib64/libarcsoft_supernight.so | \
-        vendor/lib64/libtriplecam_optical_zoom_control.so | vendor/lib64/libtriplecam_video_optical_zoom.so)
-            "${PATCHELF}" --replace-needed "libmpbase.so" "libmpbase_vendor.so" "${2}"
-            ;;
     esac
 }
 
